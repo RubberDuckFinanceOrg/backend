703be6dd6e2be7df049c24b51e1a4c64b30fb685
Matt-GitHub <matthewjamesbedard@outlook.com>
2021-02-09 16:22:55 -0500

    added testing framework, updated linting, updated edit income

---
+++ api/income/edit_income.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express'
+import { Edit } from '../../util/knex_models';
+import Income from './types'
+import status from '../../util/response_messages'
+import authorization from '../middleware/authorization';
+import incomeSchema from './income_schema'
+import validate from '../middleware/json_validator'
+const router = express()
+
+
+router.put('/income/:id', authorization, validate({ body: incomeSchema }), async (req: Request, res: Response) => {
+  try {
+    const id: string | undefined = req.params.id;
+    const newIncome: Income = await req.body;
+    const editIncome = await Edit('profiles', 'id', id, newIncome);
+    await status.okOrNotFound('edit', res, editIncome, 'profile')
+  } catch (err) {
+    status.catchAllError(res, err)
+  }
+}
+);
+
+export = router;
